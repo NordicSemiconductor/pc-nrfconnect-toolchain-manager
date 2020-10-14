fdb1de8cd9bd745878549fa23dcc948009147f56
Bence Frenyó <26139379+bencefr@users.noreply.github.com>
2020-10-14 14:19:26 +0200

    Update first steps guide for new SES

--- src/FirstInstall/FirstInstallInstructions.jsx
+++ src/FirstInstall/FirstInstallInstructions.jsx
@@ -37,7 +37,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { sep } from 'path';
-import os from 'os';
 
 import Alert from 'react-bootstrap/Alert';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
@@ -48,19 +47,108 @@ import { hideFirstSteps, selectedVersion } from '../Manager/managerReducer';
 import { currentInstallDir } from '../InstallDir/installDirReducer';
 
 const Ie = () => (<>{' '}<i>i.e.</i>{' '}</>);
+// eslint-disable-next-line react/button-has-type
+const Dotdotdot = () => (<button className="btn btn-secondary dotdotdot">...</button>);
 
-const FirstInstallInstructions = props => {
-    const dispatch = useDispatch();
+const OldSteps = () => {
     const version = useSelector(selectedVersion) || '<version>';
     const installDir = useSelector(currentInstallDir);
     const zephyrDir = [installDir, version, 'zephyr'].join(sep);
     const sampleDir = [zephyrDir, 'samples', 'basic'].join(sep);
-    const homeDir = os.homedir();
     const suggestedExample = 'blinky';
     const suggestExampleCmakelists = [sampleDir, suggestedExample, 'CMakeLists.txt'].join(sep);
     const boardName = 'nrf9160_pca10090';
     const suggestExampleBuildDir = [sampleDir, suggestedExample, `build_${boardName}`].join(sep);
     const exampleBoardDir = [zephyrDir, 'boards', 'arm', boardName].join(sep);
+
+    return (
+        <ul>
+            <li>
+                <b>CMakeLists.txt</b> - the location of
+                the <code>CMakeLists.txt</code> project file of the sample
+                that you want to work with<br />
+                <Ie /><code>{suggestExampleCmakelists}</code>
+            </li>
+            <li>
+                <b>Board Directory</b> - the location of the board description
+                of the board for which to build the project<br />
+                <Ie /><code>{exampleBoardDir}</code>
+            </li>
+            <li>
+                <b>Board Name</b> - the board name (select from the list that
+                is populated based on the board directory)<br />
+                <Ie /><code>{boardName}</code>
+            </li>
+            <li>
+                <b>Build Directory</b> - the directory in which to run the build
+                (automatically filled based on the board name, but you can
+                specify a different directory)<br />
+                <Ie /><code>{suggestExampleBuildDir}</code>
+            </li>
+            <li>
+                <b>Clean Build Directory</b> - select this option to ensure
+                that you are not building with an outdated build cache
+            </li>
+        </ul>
+    );
+};
+
+const NewSteps = () => (
+    <ul>
+        <li>
+            <b>nRF Connect SDK Release</b> - Select the NCS version that you
+            want to work with.<br />
+            The drop-down list contains the current version of all NCS
+            installation directories that SES knows about.
+        </li>
+        <li>
+            <b>nRF Connect Toolchain Version</b> - Select the version of the
+            toolchain that works with the selected NCS version.<br />
+            The drop-down list contains only toolchain versions that are
+            compatible with the selected NCS version.
+        </li>
+        <li>
+            <b>Projects</b> - Select the project that you want to work with.
+            <br />
+            The drop-down list contains a selection of samples and
+            applications from the sdk-nrf and sdk-zephyr repositories.
+            Select any of the checkboxes underneath to add the samples from
+            that area to the drop-down list. To add projects to the
+            drop-down list, for example, your own custom projects, click
+            {' '}<Dotdotdot /> and select the folder that contains the
+            projects that you want to add.
+        </li>
+        <li>
+            <b>Board Name</b> - Select the board that you want to work with.
+            <br />
+            The drop-down list contains the build targets for all Nordic
+            Semiconductor boards that are defined in the sdk-nrf and
+            sdk-zephyr repositories. Select any of the checkboxes underneath
+            to add the build targets from that area to the drop-down list.
+            To add build targets to the drop-down list, for example, targets
+            for your own custom board, click <Dotdotdot /> and select the
+            folder that contains the board definitions.
+        </li>
+        <li>
+            <b>Build Directory</b> - Select the folder in which to run the
+            build. The field is filled automatically based on the selected
+            board name, but you can specify a different directory.<br />
+        </li>
+        <li>
+            <b>Clean Build Directory</b> - Select this option to ensure that
+            you are not building with an outdated build cache.
+        </li>
+        <li>
+            <b>Extended Settings</b> - Select this option to display a field
+            where you can specify additional CMake options to be used for
+            building.
+        </li>
+    </ul>
+);
+
+const FirstInstallInstructions = props => {
+    const dispatch = useDispatch();
+    const version = useSelector(selectedVersion) || '<version>';
 
     return (
         <div {...props}>
@@ -81,44 +169,12 @@ const FirstInstallInstructions = props => {
                         complete the following steps to build nRF Connect SDK projects with SES.
                     </p>
                     <ol>
-                        <li hidden>
-                            Go to <code>{sampleDir}</code> and copy one of the directories
-                            (we suggest the classic <code>{suggestedExample}</code>) to
-                            a directory of your liking, e.g. <code>{homeDir}</code>.
-                        </li>
                         <li>Start SEGGER Embedded Studio by clicking <b>Open IDE</b></li>
                         <li>Select <code>File → Open nRF Connect SDK Project…</code></li>
                         <li>
                             To import a project into SES, you must specify the following
                             information:
-                            <ul>
-                                <li>
-                                    <b>CMakeLists.txt</b> - the location of
-                                    the <code>CMakeLists.txt</code> project file of the sample
-                                    that you want to work with<br />
-                                    <Ie /><code>{suggestExampleCmakelists}</code>
-                                </li>
-                                <li>
-                                    <b>Board Directory</b> - the location of the board description
-                                    of the board for which to build the project<br />
-                                    <Ie /><code>{exampleBoardDir}</code>
-                                </li>
-                                <li>
-                                    <b>Board Name</b> - the board name (select from the list that
-                                    is populated based on the board directory)<br />
-                                    <Ie /><code>{boardName}</code>
-                                </li>
-                                <li>
-                                    <b>Build Directory</b> - the directory in which to run the build
-                                    (automatically filled based on the board name, but you can
-                                    specify a different directory)<br />
-                                    <Ie /><code>{suggestExampleBuildDir}</code>
-                                </li>
-                                <li>
-                                    <b>Clean Build Directory</b> - select this option to ensure
-                                    that you are not building with an outdated build cache
-                                </li>
-                            </ul>
+                            {version.match(/v1.[23]/) ? <OldSteps /> : <NewSteps />}
                         </li>
                         <li>
                             Click <b>OK</b> to import the project into SES. You can now work with
