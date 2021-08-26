3643a8a30d16415fc3012c199b2a36063a800e82
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2021-08-26 11:24:24 +0200

    Add some functionality to install extensions

--- src/Manager/vscode.ts
+++ src/Manager/vscode.ts
@@ -1,32 +1,70 @@
 import { spawnSync } from 'child_process';
 
 // Not sure where to get this from yet.
-const EXTENSION_IDENTIFIER = 'nordic-semiconductor.nrf-connect';
+const REQUIRED_EXTENSIONS = [
+    'nordic-semiconductor.nrf-connect',
+    'marus25.cortex-debug',
+];
+const RECOMENDED_EXTENSIONS = [
+    'luveti.kconfig',
+    'plorefice.devicetree',
+    'ms-vscode.cpptools',
+];
 
 export enum VsCodeStatus {
     INSTALLED,
-    EXTENSION_MISSING,
+    EXTENSIONS_MISSING,
     NOT_INSTALLED,
 }
 
 export function getVsCodeStatus() {
+    const extensions = listInstalledExtensions();
+
+    if (extensions === null) {
+        return VsCodeStatus.NOT_INSTALLED;
+    }
+
+    const hasRequiredExtensions = REQUIRED_EXTENSIONS.every(extension =>
+        extensions.includes(extension)
+    );
+
+    return hasRequiredExtensions
+        ? VsCodeStatus.INSTALLED
+        : VsCodeStatus.EXTENSIONS_MISSING;
+}
+
+export function installExtensions(fromVsix = false) {
+    const existing = listInstalledExtensions();
+    const missing = [...REQUIRED_EXTENSIONS, ...RECOMENDED_EXTENSIONS].filter(
+        identifier => !existing?.includes(identifier)
+    );
+    missing.forEach(extension => installExtension(extension, fromVsix));
+}
+
+function installExtension(identifier: string, fromVsix: boolean) {
+    const pathOrIdentifier = fromVsix ? getPath(identifier) : identifier;
+
+    return spawnSync('code', ['--install-extension', pathOrIdentifier], {
+        shell: true,
+    }).status;
+}
+
+function listInstalledExtensions() {
     const { stdout, status, error } = spawnSync('code', ['--list-extensions'], {
         shell: true,
     });
 
     if (error || status !== 0) {
-        return VsCodeStatus.NOT_INSTALLED;
+        return null;
     }
 
-    const extensions = Array.from(stdout)
+    return Array.from(stdout)
         .map(character => String.fromCharCode(character))
         .join('')
         .trim()
         .split('\n');
+}
 
-    const hasNrfConnectExtension = extensions.includes(EXTENSION_IDENTIFIER);
-
-    return hasNrfConnectExtension
-        ? VsCodeStatus.INSTALLED
-        : VsCodeStatus.EXTENSION_MISSING;
+function getPath(identifier: string) {
+    return identifier;
 }
