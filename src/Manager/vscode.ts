5f5f67abd0338529afd20ae3a55baa7dd4e997b6
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2021-08-26 11:34:21 +0200

    Fix some pr coments

--- src/Manager/vscode.ts
+++ src/Manager/vscode.ts
@@ -1,3 +1,39 @@
+/* Copyright (c) 2015 - 2020, Nordic Semiconductor ASA
+ *
+ * All rights reserved.
+ *
+ * Use in source and binary forms, redistribution in binary form only, with
+ * or without modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions in binary form, except as embedded into a Nordic
+ *    Semiconductor ASA integrated circuit in a product or a software update for
+ *    such product, must reproduce the above copyright notice, this list of
+ *    conditions and the following disclaimer in the documentation and/or other
+ *    materials provided with the distribution.
+ *
+ * 2. Neither the name of Nordic Semiconductor ASA nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * 3. This software, with or without modification, must only be used with a Nordic
+ *    Semiconductor ASA integrated circuit.
+ *
+ * 4. Any software provided in binary form under this license must not be reverse
+ *    engineered, decompiled, modified and/or disassembled.
+ *
+ * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import { spawnSync } from 'child_process';
 
 // Not sure where to get this from yet.
@@ -6,6 +42,7 @@ const REQUIRED_EXTENSIONS = [
     'marus25.cortex-debug',
 ];
 const RECOMENDED_EXTENSIONS = [
+    'nordic-semiconductor.nrf-terminal',
     'luveti.kconfig',
     'plorefice.devicetree',
     'ms-vscode.cpptools',
@@ -17,7 +54,7 @@ export enum VsCodeStatus {
     NOT_INSTALLED,
 }
 
-export function getVsCodeStatus() {
+export const getVsCodeStatus = () => {
     const extensions = listInstalledExtensions();
 
     if (extensions === null) {
@@ -31,40 +68,37 @@ export function getVsCodeStatus() {
     return hasRequiredExtensions
         ? VsCodeStatus.INSTALLED
         : VsCodeStatus.EXTENSIONS_MISSING;
-}
+};
 
-export function installExtensions(fromVsix = false) {
+export const installExtensions = (fromVsix = false) => {
     const existing = listInstalledExtensions();
     const missing = [...REQUIRED_EXTENSIONS, ...RECOMENDED_EXTENSIONS].filter(
         identifier => !existing?.includes(identifier)
     );
     missing.forEach(extension => installExtension(extension, fromVsix));
-}
+};
 
-function installExtension(identifier: string, fromVsix: boolean) {
+const installExtension = (identifier: string, fromVsix: boolean) => {
     const pathOrIdentifier = fromVsix ? getPath(identifier) : identifier;
 
     return spawnSync('code', ['--install-extension', pathOrIdentifier], {
         shell: true,
     }).status;
-}
+};
 
-function listInstalledExtensions() {
+const listInstalledExtensions = () => {
     const { stdout, status, error } = spawnSync('code', ['--list-extensions'], {
         shell: true,
+        encoding: 'utf-8',
     });
 
     if (error || status !== 0) {
         return null;
     }
 
-    return Array.from(stdout)
-        .map(character => String.fromCharCode(character))
-        .join('')
-        .trim()
-        .split('\n');
-}
+    return stdout.trim().split('\n');
+};
 
-function getPath(identifier: string) {
+const getPath = (identifier: string) => {
     return identifier;
-}
+};
