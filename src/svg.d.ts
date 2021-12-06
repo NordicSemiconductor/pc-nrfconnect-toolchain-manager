475551dc4e1c4b017c9dfd0c2abcb2b585cab197
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2021-12-06 12:58:52 +0100

    Update licenses

--- src/svg.d.ts
+++ src/svg.d.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
 declare module '!!@svgr/webpack!*.svg' {
     const svg: React.ElementType;
     export default svg;
