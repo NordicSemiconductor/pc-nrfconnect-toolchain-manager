9b7729e4895db7725753d5ef45ad52cfdbcd2ae6
Jonas Fritzke Emdal <jonas.emdal@nordicsemi.no>
2022-05-27 14:54:24 +0200

    Clone ncs for nrfutil

--- src/Manager/Environment/effects/cloneNcs.ts
+++ src/Manager/Environment/effects/cloneNcs.ts
@@ -10,12 +10,15 @@ import fs from 'fs';
 import fse from 'fs-extra';
 import path from 'path';
 import { logger, usageData } from 'pc-nrfconnect-shared';
+import { gt } from 'semver';
 
 import showErrorDialog from '../../../launcherActions';
 import { Dispatch } from '../../../state';
 import EventAction from '../../../usageDataActions';
+import { westInit, westUpdate } from '../../nrfUtilToolchainManager';
 import {
     finishCloningSdk,
+    isLegacyEnvironment,
     setProgress,
     startCloningSdk,
 } from '../environmentReducer';
@@ -26,115 +29,182 @@ const { spawn: remoteSpawn } = remoteRequire('child_process');
 export const cloneNcs =
     (version: string, toolchainDir: string, justUpdate: boolean) =>
     async (dispatch: Dispatch) => {
-        dispatch(startCloningSdk(version));
-        logger.info(`Cloning nRF Connect SDK ${version}`);
-        usageData.sendUsageData(
-            EventAction.CLONE_NCS,
-            `${version}; ${process.platform}; ${process.arch}`
-        );
-        const cloneTimeStart = new Date();
+        if (isLegacyEnvironment(version)) {
+            await legacyCloneNcs(dispatch, version, justUpdate, toolchainDir);
+        } else {
+            await nrfUtilCloneNcs(dispatch, version, justUpdate, toolchainDir);
+        }
+    };
 
-        try {
-            if (!justUpdate) {
-                await fse.remove(
-                    path.resolve(path.dirname(toolchainDir), '.west')
-                );
-            }
+async function legacyCloneNcs(
+    dispatch: Dispatch,
+    version: string,
+    justUpdate: boolean,
+    toolchainDir: string
+) {
+    dispatch(startCloningSdk(version));
+    logger.info(`Cloning nRF Connect SDK ${version}`);
+    usageData.sendUsageData(
+        EventAction.CLONE_NCS,
+        `${version}; ${process.platform}; ${process.arch}`
+    );
+    const cloneTimeStart = new Date();
 
-            let ncsMgr: ChildProcess;
-            const update = justUpdate ? '--just-update' : '';
-            switch (process.platform) {
-                case 'win32': {
-                    ncsMgr = spawn(
-                        path.resolve(toolchainDir, 'bin', 'bash.exe'),
-                        [
-                            '-l',
-                            '-c',
-                            `unset ZEPHYR_BASE ; ncsmgr/ncsmgr init-ncs ${update}`,
-                        ]
-                    );
-
-                    break;
-                }
-                case 'darwin': {
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    const { ZEPHYR_BASE, ...env } = process.env;
-                    const gitversion = fs
-                        .readdirSync(`${toolchainDir}/Cellar/git`)
-                        .pop();
-                    env.PATH = `${toolchainDir}/bin:${process.env.PATH}`;
-                    env.GIT_EXEC_PATH = `${toolchainDir}/Cellar/git/${gitversion}/libexec/git-core`;
-                    env.HOME = `${process.env.HOME}`;
-
-                    ncsMgr = spawn(
-                        `${toolchainDir}/ncsmgr/ncsmgr`,
-                        ['init-ncs', `${update}`],
-                        { env }
-                    );
-                    break;
-                }
-                case 'linux': {
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    const { ZEPHYR_BASE, ...env } = process.env;
-                    env.PATH = `${toolchainDir}/bin:${toolchainDir}/usr/bin:${process.env.PATH}`;
-                    env.PYTHONHOME = `${toolchainDir}/lib/python3.8`;
-                    env.PYTHONPATH = `${toolchainDir}/usr/lib/python3.8:${toolchainDir}/lib/python3.8/site-packages:${toolchainDir}/usr/lib/python3/dist-packages:${toolchainDir}/usr/lib/python3.8/lib-dynload`;
-                    env.GIT_EXEC_PATH = `${toolchainDir}/usr/lib/git-core`;
-                    env.LD_LIBRARY_PATH = `/var/lib/snapd/lib/gl:/var/lib/snapd/lib/gl32:/var/lib/snapd/void:${toolchainDir}/lib/python3.8/site-packages/.libs_cffi_backend:${toolchainDir}/lib/python3.8/site-packages/Pillow.libs:${toolchainDir}/lib/x86_64-linux-gnu:${toolchainDir}/segger_embedded_studio/bin:${toolchainDir}/usr/lib/x86_64-linux-gnu:${toolchainDir}/lib:${toolchainDir}/usr/lib:${toolchainDir}/lib/x86_64-linux-gnu:${toolchainDir}/usr/lib/x86_64-linux-gnu`;
-
-                    ncsMgr = remoteSpawn(
-                        `${toolchainDir}/ncsmgr/ncsmgr`,
-                        ['init-ncs', `${update}`],
-                        { env }
-                    );
-                    break;
-                }
-                default:
-            }
-
-            dispatch(setProgress(version, 'Initializing environment...'));
-            logger.info(`Initializing environment for ${version}`);
-            let err = '';
-            await new Promise<void>((resolve, reject) => {
-                ncsMgr.stdout?.on('data', data => {
-                    const repo = (
-                        /=== updating (\w+)/.exec(data.toString()) || []
-                    ).pop();
-                    if (repo) {
-                        dispatch(
-                            setProgress(
-                                version,
-                                `Updating ${repo} repository...`
-                            )
-                        );
-                        logger.info(
-                            `Updating ${repo} repository for ${version}`
-                        );
-                    }
-                });
-                ncsMgr.stderr?.on('data', data => {
-                    err += `${data}`;
-                });
-                ncsMgr.on('exit', code => (code ? reject(err) : resolve()));
-            });
-        } catch (error) {
-            const errorMsg = `Failed to clone the repositories: ${error}`;
-            dispatch(showErrorDialog(errorMsg));
-            usageData.sendErrorReport(errorMsg);
+    try {
+        if (!justUpdate) {
+            await fse.remove(path.resolve(path.dirname(toolchainDir), '.west'));
         }
 
-        dispatch(finishCloningSdk(version, isWestPresent(toolchainDir)));
-        usageData.sendUsageData(
-            EventAction.CLONE_NCS_SUCCESS,
-            `${version}; ${process.platform}; ${process.arch}`
+        let ncsMgr: ChildProcess;
+        const update = justUpdate ? '--just-update' : '';
+        switch (process.platform) {
+            case 'win32': {
+                ncsMgr = spawn(path.resolve(toolchainDir, 'bin', 'bash.exe'), [
+                    '-l',
+                    '-c',
+                    `unset ZEPHYR_BASE ; ncsmgr/ncsmgr init-ncs ${update}`,
+                ]);
+
+                break;
+            }
+            case 'darwin': {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                const { ZEPHYR_BASE, ...env } = process.env;
+                const gitversion = fs
+                    .readdirSync(`${toolchainDir}/Cellar/git`)
+                    .pop();
+                env.PATH = `${toolchainDir}/bin:${process.env.PATH}`;
+                env.GIT_EXEC_PATH = `${toolchainDir}/Cellar/git/${gitversion}/libexec/git-core`;
+                env.HOME = `${process.env.HOME}`;
+
+                ncsMgr = spawn(
+                    `${toolchainDir}/ncsmgr/ncsmgr`,
+                    ['init-ncs', `${update}`],
+                    { env }
+                );
+                break;
+            }
+            case 'linux': {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                const { ZEPHYR_BASE, ...env } = process.env;
+                env.PATH = `${toolchainDir}/bin:${toolchainDir}/usr/bin:${process.env.PATH}`;
+                env.PYTHONHOME = `${toolchainDir}/lib/python3.8`;
+                env.PYTHONPATH = `${toolchainDir}/usr/lib/python3.8:${toolchainDir}/lib/python3.8/site-packages:${toolchainDir}/usr/lib/python3/dist-packages:${toolchainDir}/usr/lib/python3.8/lib-dynload`;
+                env.GIT_EXEC_PATH = `${toolchainDir}/usr/lib/git-core`;
+                env.LD_LIBRARY_PATH = `/var/lib/snapd/lib/gl:/var/lib/snapd/lib/gl32:/var/lib/snapd/void:${toolchainDir}/lib/python3.8/site-packages/.libs_cffi_backend:${toolchainDir}/lib/python3.8/site-packages/Pillow.libs:${toolchainDir}/lib/x86_64-linux-gnu:${toolchainDir}/segger_embedded_studio/bin:${toolchainDir}/usr/lib/x86_64-linux-gnu:${toolchainDir}/lib:${toolchainDir}/usr/lib:${toolchainDir}/lib/x86_64-linux-gnu:${toolchainDir}/usr/lib/x86_64-linux-gnu`;
+
+                ncsMgr = remoteSpawn(
+                    `${toolchainDir}/ncsmgr/ncsmgr`,
+                    ['init-ncs', `${update}`],
+                    { env }
+                );
+                break;
+            }
+            default:
+        }
+
+        dispatch(setProgress(version, 'Initializing environment...'));
+        logger.info(`Initializing environment for ${version}`);
+        let err = '';
+        await new Promise<void>((resolve, reject) => {
+            ncsMgr.stdout?.on('data', data => {
+                const repo = (
+                    /=== updating (\w+)/.exec(data.toString()) || []
+                ).pop();
+                if (repo) {
+                    dispatch(
+                        setProgress(version, `Updating ${repo} repository...`)
+                    );
+                    logger.info(`Updating ${repo} repository for ${version}`);
+                }
+            });
+            ncsMgr.stderr?.on('data', data => {
+                err += `${data}`;
+            });
+            ncsMgr.on('exit', code => (code ? reject(err) : resolve()));
+        });
+    } catch (error) {
+        const errorMsg = `Failed to clone the repositories: ${error}`;
+        dispatch(showErrorDialog(errorMsg));
+        usageData.sendErrorReport(errorMsg);
+    }
+
+    dispatch(finishCloningSdk(version, isWestPresent(toolchainDir)));
+    usageData.sendUsageData(
+        EventAction.CLONE_NCS_SUCCESS,
+        `${version}; ${process.platform}; ${process.arch}`
+    );
+    usageData.sendUsageData(
+        EventAction.CLONE_NCS_TIME,
+        `${calculateTimeConsumed(cloneTimeStart)} min; ${version}`
+    );
+    logger.info(
+        `Finished cloning version ${version} of the nRF Connect SDK after approximately ${calculateTimeConsumed(
+            cloneTimeStart
+        )} minute(s)`
+    );
+}
+
+async function nrfUtilCloneNcs(
+    dispatch: Dispatch,
+    version: string,
+    justUpdate: boolean,
+    toolchainDir: string
+) {
+    dispatch(startCloningSdk(version));
+    logger.info(`Cloning nRF Connect SDK ${version}`);
+
+    usageData.sendUsageData(
+        EventAction.CLONE_NCS,
+        `${version}; ${process.platform}; ${process.arch}`
+    );
+    const cloneTimeStart = new Date();
+
+    try {
+        if (!justUpdate) {
+            await fse.remove(path.resolve(path.dirname(toolchainDir), '.west'));
+        }
+        setProgress(version, `Initializing west...`);
+        await westInit();
+
+        dispatch(setProgress(version, 'Initializing environment...'));
+        logger.info(`Initializing environment for ${version}`);
+
+        await westUpdate(
+            update => {
+                const repo = (
+                    /=== updating (\w+)/.exec(update.toString()) || []
+                ).slice(-1)[0];
+                if (repo) {
+                    dispatch(
+                        setProgress(version, `Updating ${repo} repository...`)
+                    );
+                    logger.info(`Updating ${repo} repository for ${version}`);
+                }
+            },
+            error => {
+                console.error(error.toString());
+            }
         );
-        usageData.sendUsageData(
-            EventAction.CLONE_NCS_TIME,
-            `${calculateTimeConsumed(cloneTimeStart)} min; ${version}`
-        );
-        logger.info(
-            `Finished cloning version ${version} of the nRF Connect SDK after approximately ${calculateTimeConsumed(
-                cloneTimeStart
-            )} minute(s)`
-        );
-    };
+    } catch (error) {
+        const errorMsg = `Failed to clone the repositories: ${error}`;
+        dispatch(showErrorDialog(errorMsg));
+        usageData.sendErrorReport(errorMsg);
+    }
+
+    dispatch(finishCloningSdk(version, isWestPresent(toolchainDir)));
+
+    usageData.sendUsageData(
+        EventAction.CLONE_NCS_SUCCESS,
+        `${version}; ${process.platform}; ${process.arch}`
+    );
+    usageData.sendUsageData(
+        EventAction.CLONE_NCS_TIME,
+        `${calculateTimeConsumed(cloneTimeStart)} min; ${version}`
+    );
+    logger.info(
+        `Finished cloning version ${version} of the nRF Connect SDK after approximately ${calculateTimeConsumed(
+            cloneTimeStart
+        )} minute(s)`
+    );
+}
