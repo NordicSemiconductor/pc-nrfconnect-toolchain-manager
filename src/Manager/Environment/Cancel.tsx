2863cdb086e09cdf4866750796caa68733d88962
Pascal Pickel <Pascal.Pickel@nordicsemi.no>
2022-06-24 10:55:21 +0200

    feat: make the cancel button it's own component
    Signed-off-by: Pascal Pickel <Pascal.Pickel@nordicsemi.no>

---
+++ src/Manager/Environment/Cancel.tsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+import React from 'react';
+import { usageData } from 'pc-nrfconnect-shared';
+
+import { Environment } from '../../state';
+import Button from './Button';
+import { isDoingFreshInstall, isLegacyEnvironment } from './environmentReducer';
+
+type Props = { environment: Environment };
+
+const Cancel = ({ environment }: Props) => {
+    const onCancel = () => {
+        environment.abortController.abort();
+        usageData.sendUsageData('Cancel installation', environment.version);
+    };
+
+    if (
+        isLegacyEnvironment(environment.version) ||
+        !isDoingFreshInstall(environment)
+    )
+        return null;
+
+    return (
+        <Button
+            icon="x-mdi-briefcase-download-outline"
+            onClick={onCancel}
+            label="Cancel"
+            variant="secondary"
+        />
+    );
+};
+
+export default Cancel;
