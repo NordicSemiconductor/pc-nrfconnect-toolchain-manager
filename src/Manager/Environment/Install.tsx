af5ab532423c82ef8d0bd0df610bf02bc5b5cd86
kylebonnici <kylebonnici@hotmail.com>
2024-06-12 15:58:32 +0200

    Feat: add warning for NCS 2.7

--- src/Manager/Environment/Install.tsx
+++ src/Manager/Environment/Install.tsx
@@ -4,8 +4,13 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+    DialogButton,
+    GenericDialog,
+    openUrl,
+} from '@nordicsemiconductor/pc-nrfconnect-shared';
 
 import { showConfirmInstallDirDialog } from '../../InstallDir/installDirSlice';
 import { Environment } from '../../state';
@@ -14,34 +19,83 @@ import Button from './Button';
 import { install } from './effects/installEnvironment';
 import { isOnlyAvailable, version } from './environmentReducer';
 
-type Props = { environment: Environment };
+type Props = { environment: Environment; showExperimentalWarning: boolean };
 
-const Install = ({ environment }: Props) => {
+const Install = ({ environment, showExperimentalWarning }: Props) => {
     const dispatch = useDispatch();
     const anyInProgress = useSelector(isAnyToolchainInProgress);
+    const [showExperimentalDialog, setShowExperimentalDialog] = useState(false);
 
     const { platform } = process;
-    const onClick = (() => {
+    const onClick = () => {
         switch (platform) {
             case 'darwin':
-                return () => dispatch(install(environment, false));
+                return dispatch(install(environment, false));
             case 'linux':
-                return () =>
-                    dispatch(showConfirmInstallDirDialog(version(environment)));
+                return dispatch(
+                    showConfirmInstallDirDialog(version(environment))
+                );
             case 'win32':
-                return () =>
-                    dispatch(showConfirmInstallDirDialog(version(environment)));
+                return dispatch(
+                    showConfirmInstallDirDialog(version(environment))
+                );
         }
-    })();
+    };
 
     return !isOnlyAvailable(environment) ? null : (
-        <Button
-            icon="x-mdi-briefcase-download-outline"
-            onClick={onClick}
-            label="Install"
-            variant="secondary"
-            disabled={anyInProgress}
-        />
+        <>
+            <GenericDialog
+                isVisible={showExperimentalDialog}
+                headerIcon="alert"
+                title="Experimental Toolchain and SDK"
+                footer={
+                    <>
+                        <DialogButton
+                            variant="danger"
+                            onClick={() => {
+                                setShowExperimentalDialog(false);
+                                onClick();
+                            }}
+                        >
+                            Accept the risks and install
+                        </DialogButton>
+                        <DialogButton
+                            onClick={() => {
+                                setShowExperimentalDialog(false);
+                                openUrl(
+                                    'https://github.com/microsoft/vscode-docs/blob/vnext/release-notes/v1_63.md#pre-release-extensions'
+                                );
+                            }}
+                        >
+                            How use VSCode pre-release
+                        </DialogButton>
+                        <DialogButton
+                            onClick={() => setShowExperimentalDialog(false)}
+                        >
+                            Cancel
+                        </DialogButton>
+                    </>
+                }
+            >
+                Support for nRF Connect SDK {environment.version} in nRF Connect
+                for VS Code is experimental. VS Code users that need{' '}
+                {environment.version} should use the preview version of the
+                extension, please refer to VS Code for details.
+            </GenericDialog>
+            <Button
+                icon="x-mdi-briefcase-download-outline"
+                onClick={() => {
+                    if (showExperimentalWarning) {
+                        setShowExperimentalDialog(true);
+                    } else {
+                        onClick();
+                    }
+                }}
+                label="Install"
+                variant="secondary"
+                disabled={anyInProgress}
+            />
+        </>
     );
 };
 
